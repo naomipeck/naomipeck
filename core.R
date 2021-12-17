9d0f84398aec1d16b2dadf9a7052768d3e04e058
Naomi <naomipeck@hotmail.com>
2021-12-17 14:04:54 +0100

    commented on code

--- core.R
+++ core.R
@@ -1,23 +1,33 @@
 library(blogdown)
+
+# create new site
 new_site(theme = "wowchemy/starter-academic")
 
+#load/stop loading site
 blogdown::serve_site()
 blogdown::stop_server()
 
+#Create new post
 blogdown::new_post(title = "Boundary Cues Coding in Praat",
                    ext = '.Rmarkdown',
                    subdir = "post")
 
+# adjust settings 
+# make sure to restart R session for changes to take affect
 blogdown::config_Rprofile() 
 
+#sanity checks
 blogdown::check_gitignore()
 blogdown::check_config()
 blogdown::check_site()
 blogdown::check_netlify()
 blogdown::check_hugo()
+blogdown::check_content()
 
 blogdown::remove_hugo()
 
+
+#shortcuts to open base files
 rstudioapi::navigateToFile("config.yaml", line = 3)
 
 rstudioapi::navigateToFile("config/_default/params.toml")
